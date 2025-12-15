345777ee0e001d38b0e65314238812840fce88e1
Durren-it <micheledelvescovo@gmail.com>
2025-12-15 13:21:54 +0100

    Quick fix imports

--- src/main/java/com/eventpro/catering/controller/UserController.java
+++ src/main/java/com/eventpro/catering/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.eventpro.catering.controller;
 
 import org.springframework.http.ResponseEntity;
-import org. springframework.security.core.Authentication;
-import org.springframework.security. core.context.SecurityContextHolder;
-import org.springframework. web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web. bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
